b1a7dacbd567ac45bc886c4722acfb4c5966fca1
Layne Sadler <layne@macbook.local>
2021-02-03 15:16:16 -0500

    plotly.js bundled w docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,14 @@ extensions = [
 	, 'sphinx_rtd_theme'
 	, 'sphinxext.opengraph'
 ]
+# Adding these due to plotly browser error. Integrity in the "</> Copy Script Tag" button.
+# https://cdnjs.com/libraries/require.js/2.1.10
+#nbsphinx_requirejs_path = "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"
+nbsphinx_requirejs_options = {
+	"src": "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js",
+	"integrity": "sha512-VCK7oF67GXNc+J7zsu5o57jtxhLA75nSMHGaq8Q8TCOxDj4nMDw5dhQZvm9Cd9RN+3zgcodqbKcRc9gyPP8a2w==",
+	"crossorigin": "anonymous"
+}
 
 # https://nbsphinx.readthedocs.io/en/0.7.0/usage.html#suppress_warnings
 suppress_warnings = [
