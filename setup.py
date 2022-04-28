6691db97bda555114f5d373c27dcac0a56c0a995
Layne <layne.sadler@gmail.com>
2022-04-27 20:15:13 -0400

    5.0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiqc",
-    version="5.0.3",
+    version="5.0.4",
     author="Layne Sadler",
     author_email="layne.sadler@gmail.com",
     description="End-to-end machine learning on your desktop or server.",
