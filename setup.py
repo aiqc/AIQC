97e7f3a765c235356cb42987a9f5223ef20eb5a4
Layne <layne.sadler@gmail.com>
2022-05-12 16:50:21 -0400

    5.3.1 torch sample-level shuffle

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiqc",
-    version="5.3.0",
+    version="5.3.1",
     author="Layne Sadler",
     author_email="layne.sadler@gmail.com",
     description="End-to-end machine learning on your desktop or server.",
