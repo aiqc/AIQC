9f18e1e7ae40a5b081c16175dbd6b9961d8bf602
Layne <layne.sadler@gmail.com>
2022-05-13 12:36:32 -0400

    5.4.1 granular imports for speed

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiqc",
-    version="5.4.0",
+    version="5.4.1",
     author="Layne Sadler",
     author_email="layne.sadler@gmail.com",
     description="End-to-end machine learning on your desktop or server.",
