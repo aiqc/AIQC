243ef5ac1f92dffbce6e594e479340891c0ede8d
Layne <layne.sadler@gmail.com>
2022-06-01 15:09:47 -0400

    revised torch fit to accept separate train and eval data

--- aiqc/tests/torch_multi_tab.py
+++ aiqc/tests/torch_multi_tab.py
@@ -1,5 +1,6 @@
 """PyTorch Multi-label Classification with Tabular data"""
 # Internal modules
+from ..mlops import Pipeline, Input, Target, Stratifier, Experiment, Architecture, Trainer
 from .. import datum
 from ..utils.pytorch import fit
 from ..orm import *
@@ -27,80 +28,73 @@ def fn_lose(**hp):
 	return loser
 
 
-def fn_train(model, loser, optimizer, samples_train, samples_evaluate, **hp):
+def fn_train(
+	model, loser, optimizer,
+	train_features, train_label,
+	eval_features, eval_label,
+	**hp
+):
 	return fit(
 		model, loser, optimizer, 
-		samples_train, samples_evaluate,
+		train_features, train_label,
+		eval_features, eval_label,
 		epochs=10, batch_size=hp['batch_size'],  
 		metrics=[tm.Accuracy(), tm.F1Score()]
 	)
 
-def make_queue(repeat_count:int=1, fold_count:int=None, permute_count:int=3):
+
+def make_queue(repeat_count:int=1, fold_count:int=None, permute_count:int=2):
+	hyperparameters = {
+		"reduction":['mean'], "batch_size":[5]
+	}
+	
 	if (fold_count is not None):
 		file_path = datum.get_path('iris_10x.tsv')
 	else:
 		file_path = datum.get_path('iris.tsv')
 
-	d_id = Dataset.Tabular.from_path(
+	shared_dataset = Dataset.Tabular.from_path(
 		file_path = file_path
 		, source_file_format = 'tsv'
 		, dtype = None
-	).id
-	
-	label_column = 'species'
-	l_id = Label.from_dataset(dataset_id=d_id, columns=[label_column]).id
-	f_id = Feature.from_dataset(dataset_id=d_id, exclude_columns=[label_column]).id
-
-	if (fold_count is not None):
-		size_test = 0.25
-		size_validation = None
-	elif (fold_count is None):
-		size_test = 0.18
-		size_validation = 0.14
-
-	s_id = Splitset.make(
-		feature_ids = [f_id]
-		, label_id = l_id
-		, size_test = size_test
-		, size_validation = size_validation
-	).id
-
-	if (fold_count is not None):
-		fs_id = Foldset.from_splitset(splitset_id=s_id, fold_count=fold_count).id
-	else:
-		fs_id = None
-
-	LabelCoder.from_label(label_id=l_id, sklearn_preprocess=OrdinalEncoder())
-
-	e_id = Encoderset.from_feature(feature_id=f_id).id
-	FeatureCoder.from_encoderset(
-		encoderset_id = e_id
-		, sklearn_preprocess = StandardScaler(copy=False)
-		, dtypes = ['float64']
 	)
 
-	a_id = Algorithm.make(
-		library = "pytorch"
-		, analysis_type = "classification_multi"
-		, fn_build = fn_build
-		, fn_train = fn_train
-        , fn_lose = fn_lose
-	).id
+	pipeline = Pipeline(
+		Input(
+			dataset  = shared_dataset,
+			encoders = Input.Encoder(
+				StandardScaler(copy=False),
+				dtypes = ['float64']
+			)
+		),
 
-	hyperparameters = {
-		"reduction":['mean'], "batch_size":[5]
-	}
-	h_id = Hyperparamset.from_algorithm(
-		algorithm_id=a_id, hyperparameters=hyperparameters
-	).id
+		Target(
+			dataset = shared_dataset,
+			column  = 'species',
+			encoder = Target.Encoder(OrdinalEncoder())
+		),
 
-	queue = Queue.from_algorithm(
-		algorithm_id = a_id
-		, splitset_id = s_id
-		, foldset_id = fs_id
-		, hyperparamset_id = h_id
-		, repeat_count = repeat_count
-		, permute_count = permute_count
+		Stratifier(
+			size_test       = 0.11, 
+			size_validation = 0.21,
+			fold_count      = fold_count
+		)    
 	)
-	return queue
 
+	experiment = Experiment(
+		Architecture(
+			library           = "pytorch"
+			, analysis_type   = "classification_multi"
+			, fn_build        = fn_build
+			, fn_train        = fn_train
+			, hyperparameters = hyperparameters
+		),
+		
+		Trainer(
+			pipeline_id       = pipeline.id
+			, repeat_count    = repeat_count
+			, permute_count   = permute_count
+			, search_percent  = None
+		)
+	)
+	return experiment
--- aiqc/utils/pytorch.py
+++ aiqc/utils/pytorch.py
@@ -43,7 +43,7 @@ def predict_multiclass(model, features):
 	return prediction, probabilities
 
 def predict_regression(model, features):
-	prediction = model(features).detach().numpy()###
+	prediction = model(features).detach().numpy()
 	return prediction
 
 
@@ -71,19 +71,26 @@ def drop_invalidBatch(
 	return batched_data
 
 
-def shuffle_samples(features:list, labels:list):
+def shuffle_samples(features:list, label:object):
 	"""Assumes that the first index represents the batch."""
-	rand_idx = list(range(len(labels)))
+	rand_idx = list(range(len(label)))
 	shuffle(rand_idx)
+	
+	label = torch.stack([label[i] for i in rand_idx]).to(torch.float)
 
-	features = torch.stack([features[i] for i in rand_idx]).to(torch.float)
-	labels = torch.stack([labels[i] for i in rand_idx]).to(torch.float)
-	return features, labels
+	if isinstance(features, list):
+		features = []
+		for f in features:
+			f = torch.stack([f[i] for i in rand_idx]).to(torch.float)
+			features.append(f)
+	else:
+		torch.stack([features[i] for i in rand_idx]).to(torch.float)
+	return features, label
 
 
 def batch_samples(
-	features:object, labels:object
-	, batch_size = 5, enforce_sameSize:bool=False, allow_singleSample:bool=False
+	features:object, label:object, 
+	batch_size=5, enforce_sameSize:bool=False, allow_singleSample:bool=False
 ):
 	if (batch_size==1):
 		if (allow_singleSample==False):
@@ -92,24 +99,37 @@ def batch_samples(
 			print("\nWarning - PyTorch errors are common when `batch_size==1`.")
 	
 	# split() normally returns a tuple.
-	features = list(torch.split(features, batch_size))
-	labels = list(torch.split(labels, batch_size))
+	label = list(torch.split(label, batch_size))
+	label = drop_invalidBatch(label, batch_size, enforce_sameSize, allow_singleSample)
 
-	features = drop_invalidBatch(features, batch_size, enforce_sameSize, allow_singleSample)
-	labels = drop_invalidBatch(labels, batch_size, enforce_sameSize, allow_singleSample)
-	return features, labels
+	if isinstance(features, list):
+		batched_features = []
+		for f in features:
+			f = list(torch.split(f, batch_size))
+			f = drop_invalidBatch(f, batch_size, enforce_sameSize, allow_singleSample)
+			batched_features.append(f)
+		features = []
+		for bf in batched_features:
+			for f in bf:
+				features.append(f)
+	else:
+		features = list(torch.split(features, batch_size))
+		features = drop_invalidBatch(features, batch_size, enforce_sameSize, allow_singleSample)
+	return features, label
 
 
-def shuffle_batches(features:list, labels:list):
+def shuffle_batches(features:list, label:list):
 	"""
 	- Assumes that the first index represents the batch.
 	- Makes sure batches aren't seen in same order every epoch.
 	"""
-	rand_idx = list(range(len(labels)))
+	rand_idx = list(range(len(label)))
 	shuffle(rand_idx)
+	
+	label = [label[i] for i in rand_idx]
+	# This one doesn't need anything special for multiple features
 	features = [features[i] for i in rand_idx]
-	labels = [labels[i] for i in rand_idx]
-	return features, labels
+	return features, label
 
 
 def flatten_uniColumn(tzr:object):
@@ -131,16 +151,18 @@ def float_to_int(tzr:object):
 
 
 def fit(
-	model:object, loser:object, optimizer:object,  
-	samples_train:dict, samples_evaluate:dict,
-	epochs:int=30, batch_size:int=5, 
-	enforce_sameSize:bool=True, allow_singleSample:bool=False,  
+	model:object, loser:object, optimizer:object,
+	train_features:list, train_label:object,
+	eval_features:list, eval_label:object,
+	epochs:int=30, batch_size:int=5,
+	enforce_sameSize:bool=True, allow_singleSample:bool=False,
 	metrics:list=None
 ):
 	"""
 	- This is the only user-facing function for non-manual training loops.
 	- It is designed to handle all supervised scenarios.
 	- Have not tested this with self-supervised where 2D+ compared to 2D+
+	- Need to write a test for multi-modal where `train_features` is a list of tensors.
 	"""
 	# Mirrors `tf.keras.model.History.history` schema for use with `Predictor.plot_learning_curve()`
 	history = dict(loss=list(), val_loss=list())
@@ -155,11 +177,17 @@ def fit(
 			val_name = f"val_{name}"
 			history[val_name] = list()
 			metrics_keys.append((name, val_name))
-	
-	shuffled_features, shuffled_labels = shuffle_samples(samples_train['features'], samples_train['labels'])
+
+	train_features, train_label = shuffle_samples(train_features, train_label)
+	eval_features, eval_label = shuffle_samples(eval_features, eval_label)
 	## --- Prepare mini batches for analysis ---
-	batched_features, batched_labels = batch_samples(
-		shuffled_features, shuffled_labels,
+	# The variables below are reassigned because we need the above variables for epoch metrics
+	trainFeatures_batched, trainLabel_batched = batch_samples(
+		train_features, train_label,
+		batch_size=batch_size, enforce_sameSize=enforce_sameSize, allow_singleSample=allow_singleSample
+	)
+	evalFeatures_batched, evalLabel_batched = batch_samples(
+		eval_features, eval_label,
 		batch_size=batch_size, enforce_sameSize=enforce_sameSize, allow_singleSample=allow_singleSample
 	)
 	"""
@@ -176,19 +204,21 @@ def fit(
 	"""
 	## --- Training Loop ---
 	for epoch in range(epochs):
-		batched_features, batched_labels = shuffle_batches(batched_features, batched_labels)
+		trainFeatures_batched, trainLabel_batched = shuffle_batches(trainFeatures_batched, trainLabel_batched)
+		evalFeatures_batched, evalLabel_batched = shuffle_batches(evalFeatures_batched, evalLabel_batched)
+
 		## --- Batch Training ---
-		for i, batch in enumerate(batched_features):
+		for e, batch_features in enumerate(trainFeatures_batched):
 			# Make raw (unlabeled) predictions.
-			batch_probability = model(batched_features[i])
-			batch_labels = batched_labels[i]
+			batch_label = trainLabel_batched[e]
+			batch_probability = model(batch_features)
 			try:
-				batch_loss = loser(batch_probability, batch_labels)
+				batch_loss = loser(batch_probability, batch_label)
 			except:
 				# Known exception: multi classify fails on 2D and floats
-				batch_labels = flatten_uniColumn(batch_labels)
-				batch_labels = float_to_int(batch_labels)
-				batch_loss = loser(batch_probability, batch_labels)
+				batch_label = flatten_uniColumn(batch_label)
+				batch_label = float_to_int(batch_label)
+				batch_loss = loser(batch_probability, batch_label)
 			# Backpropagation.
 			optimizer.zero_grad()
 			batch_loss.backward()
@@ -196,42 +226,42 @@ def fit(
 
 		## --- Epoch Loss ---
 		# Known exception: multi classify fails on floats
-		train_probability = model(samples_train['features'])
+		train_probability = model(train_features)
 		train_probability = flatten_uniColumn(train_probability)
-		train_labels = flatten_uniColumn(samples_train['labels'])
+		train_label = flatten_uniColumn(train_label)
 		try:
-			train_loss = loser(train_probability, train_labels)
+			train_loss = loser(train_probability, train_label)
 		except:
-			train_labels = float_to_int(train_labels)
-			train_loss = loser(train_probability, train_labels)
+			train_label = float_to_int(train_label)
+			train_loss = loser(train_probability, train_label)
 		history['loss'].append(float(train_loss))
 
-		eval_probability = model(samples_evaluate['features'])
+		eval_probability = model(eval_features)
 		eval_probability = flatten_uniColumn(eval_probability)
-		eval_labels = flatten_uniColumn(samples_evaluate['labels'])
+		eval_label = flatten_uniColumn(eval_label)
 		try:
-			eval_loss = loser(eval_probability, eval_labels)
+			eval_loss = loser(eval_probability, eval_label)
 		except:
-			eval_labels = float_to_int(eval_labels)
-			eval_loss = loser(eval_probability, eval_labels)
+			eval_label = float_to_int(eval_label)
+			eval_loss = loser(eval_probability, eval_label)
 		history['val_loss'].append(float(eval_loss))
 
 		## --- Epoch Metrics ---
 		# Known exception: binary classify accuracy fails on floats.
 		for i, m in enumerate(metrics):
 			try:
-				train_m = m(train_probability, train_labels)
+				train_m = m(train_probability, train_label)
 			except:
-				train_labels = float_to_int(train_labels)
-				train_m = m(train_probability, train_labels)
+				train_label = float_to_int(train_label)
+				train_m = m(train_probability, train_label)
 			metrics_key = metrics_keys[i][0]
 			history[metrics_key].append(float(train_m))
 
 			try:
-				eval_m = m(eval_probability, eval_labels)
+				eval_m = m(eval_probability, eval_label)
 			except:
-				eval_labels = float_to_int(eval_labels)
-				eval_m = m(eval_probability, eval_labels)
+				eval_label = float_to_int(eval_label)
+				eval_m = m(eval_probability, eval_label)
 			metrics_key = metrics_keys[i][1]
 			history[metrics_key].append(float(eval_m))
 	return model, history
