1128035d2931bd9775f045a23a65d3435218063f
Layne <layne.sadler@gmail.com>
2022-06-10 12:42:34 -0400

    test new doc table

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,21 @@
-# *** REFRESHING SIDEBAR MENU ***
-# When you change menu/ page tiles. Run `make html` with this line uncommented and again with it commented:
+"""
+	<!> REFRESHING SIDEBAR MENU <!>
+	When you change menu/ file names, you need to run `make html` 
+	with this line uncommented, and then run it again with it commented:
+"""
 #html_sidebars = { '**': ['globaltoc.html'] }
 
-project = 'AIQC'
+project   = 'AIQC'
 copyright = '2020, Team AIQC'
-author = 'Team AIQC'
+author    = 'Team AIQC'
 
-# -- Plugins -------------------------------------------------
-# https://nbsphinx.readthedocs.io/en/0.8.0/
-# These are pip packages. See `docs/requirements.txt` 
-# Which is called by `.readthedocs.yml`
+"""
+	====== PLUGINS ======
+	https://nbsphinx.readthedocs.io/en/0.8.0/
+	These are pip packages. See `docs/requirements.txt` 
+	Which is called by `.readthedocs.yml`
+	https://nbsphinx.readthedocs.io/en/0.7.0/usage.html#suppress_warnings
+"""
 import sphinx_rtd_theme
 extensions = [
 	'nbsphinx'
@@ -18,22 +24,24 @@ extensions = [
 	, 'sphinxext.opengraph'
 	, 'sphinxcontrib.youtube'
 ]
-# https://nbsphinx.readthedocs.io/en/0.7.0/usage.html#suppress_warnings
 suppress_warnings  = ['nbsphinx']
 highlight_language = 'python3'
 
-
-# -- Assets -------------------------------------------------
-# Acts like .gitignore. nbsphinx automatically excludes '.ipynb_checkpoints'
+"""
+	====== ASSETS ======
+	Acts like .gitignore. nbsphinx automatically excludes '.ipynb_checkpoints'
+"""
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Untitled.ipynb']
 templates_path   = ['_templates']
 html_static_path = ['_static']
 html_css_files   = ['css/custom.css']
 html_js_files    = []
 
+"""
+	====== OPTIONS ======
+	SVG logo: stackoverflow.com/questions/59215996/how-to-add-a-logo-to-my-readthedocs-logo-rendering-at-0px-wide
+"""
 
-# -- Options -------------------------------------------------
-# SVG logo: stackoverflow.com/questions/59215996/how-to-add-a-logo-to-my-readthedocs-logo-rendering-at-0px-wide
 html_theme          = "sphinx_rtd_theme" # see extension and `import` above.
 html_logo           = '_static/images/web/aiqc_logo_square_blues_transparent.svg'
 html_favicon        = '_static/images/web/favicon.ico'
@@ -49,11 +57,12 @@ html_theme_options  = dict(
     , prev_next_buttons_location = None
 )
 
-
-# -- <head><meta> title & links -----------------------------------------
-# https://github.com/wpilibsuite/sphinxext-opengraph
-# https://www.linkedin.com/pulse/how-clear-linkedin-link-preview-cache-ananda-kannan-p/
-# https://www.linkedin.com/post-inspector/
+"""
+	====== HTML HEAD & META ======
+	https://github.com/wpilibsuite/sphinxext-opengraph
+	https://www.linkedin.com/pulse/how-clear-linkedin-link-preview-cache-ananda-kannan-p/
+	https://www.linkedin.com/post-inspector/
+"""
 ogp_site_url         = "https://aiqc.readthedocs.io/"
 ogp_site_name        = "AIQC"
 ogp_image            = "https://raw.githubusercontent.com/aiqc/aiqc/main/docs/_static/images/web/meta_image_tall_rect.png"
